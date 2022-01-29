c0b858350fd5047eb91c9dc889dda3eceb1e7064
rajpatel2435 <81604941+rajpatel2435@users.noreply.github.com>
2022-01-28 19:35:38 -0500

    Add files via upload

---
+++ assignment 4/speakGoodBye.js
@@ -0,0 +1,13 @@
+(function(window){
+	let byeSpeaker = {};
+	let speakWord = "Good Bye";
+	byeSpeaker.speak = function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
+
+window.byeSpeaker = byeSpeaker;
+
+})
+(window);
+
+
